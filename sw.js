c2d96d4f3ccaeb75ecf342a91fcdc76228cf4b93
rsnwms2tpm-lang <rsnwms2tpm@privaterelay.appleid.com>
2026-09-12 09:38:15 +0100

    Bump Business Hunter cache after sync credential fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="business-hunter-v4";
+const CACHE="business-hunter-v5";
 const ASSETS=["./","./index.html","./manifest.webmanifest"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>{e.waitUntil((async()=>{for(const k of await caches.keys())if(k!==CACHE)await caches.delete(k);await self.clients.claim()})())});
